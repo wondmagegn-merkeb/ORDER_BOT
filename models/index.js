87ea014fd22569370a712a95efb13d46f0a48b07
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-15 19:34:13 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+// Import all models
 const Admin = require('./Admin');
 const AdminAuditLog = require('./AdminAuditLog');
 const User = require('./User');
@@ -8,44 +9,90 @@ const FoodUpdateLog = require('./FoodUpdateLog');
 const Order = require('./Order');
 const OrderUpdateLog = require('./OrderUpdateLog');
 
-// Associations between models
+/**
+ * ===============================
+ * Model Relationships / Associations
+ * ===============================
+ */
 
-// Admin has many AdminAuditLogs
-Admin.hasMany(AdminAuditLog, {
-  foreignKey: 'performedBy',  // Foreign key in AdminAuditLog that references Admin
-});
-
-// AdminAuditLog belongs to Admin
-AdminAuditLog.belongsTo(Admin, {
-  foreignKey: 'performedBy',  // Foreign key in AdminAuditLog that references Admin
-});
-
-
-// FoodCategory has many Foods
+// FoodCategory ↔ Food
 FoodCategory.hasMany(Food, { foreignKey: 'categoryId' });
 Food.belongsTo(FoodCategory, { foreignKey: 'categoryId' });
 
-// Food has many FoodUpdateLogs (for dynamic tracking of changes)
+// Food ↔ FoodUpdateLog
 Food.hasMany(FoodUpdateLog, { foreignKey: 'foodId' });
 FoodUpdateLog.belongsTo(Food, { foreignKey: 'foodId' });
 
-// Associations with Order model
-Order.hasMany(OrderUpdateLog, { foreignKey: 'orderId' });
-OrderUpdateLog.belongsTo(Order, { foreignKey: 'orderId' });
-
-// User has many Orders
+// User ↔ Order
 User.hasMany(Order, { foreignKey: 'userId' });
 Order.belongsTo(User, { foreignKey: 'userId' });
 
-// Admin can update an Order, so add the association without using alias
-Order.belongsTo(Admin, { foreignKey: 'updatedBy' });
-Admin.hasMany(Order, { foreignKey: 'updatedBy' });
+// Order ↔ OrderUpdateLog
+Order.hasMany(OrderUpdateLog, { foreignKey: 'orderId' });
+OrderUpdateLog.belongsTo(Order, { foreignKey: 'orderId' });
 
-// Associations for UserUpdateLog
+// User ↔ UserUpdateLog
 User.hasMany(UserUpdateLog, { foreignKey: 'userId' });
 UserUpdateLog.belongsTo(User, { foreignKey: 'userId' });
 
-// Export all models so they can be used in other files
+// Admin ↔ Order (Updated By)
+Admin.hasMany(Order, { foreignKey: 'updatedBy' });
+Order.belongsTo(Admin, { foreignKey: 'updatedBy' });
+
+/**
+ * Link all 'performedBy' in log models to Admin
+ */
+Admin.hasMany(AdminAuditLog, { foreignKey: 'performedBy' });
+AdminAuditLog.belongsTo(Admin, { foreignKey: 'performedBy' });
+
+Admin.hasMany(FoodUpdateLog, { foreignKey: 'performedBy' });
+FoodUpdateLog.belongsTo(Admin, { foreignKey: 'performedBy' });
+
+Admin.hasMany(UserUpdateLog, { foreignKey: 'performedBy' });
+UserUpdateLog.belongsTo(Admin, { foreignKey: 'performedBy' });
+
+Admin.hasMany(OrderUpdateLog, { foreignKey: 'performedBy' });
+OrderUpdateLog.belongsTo(Admin, { foreignKey: 'performedBy' });
+
+/**
+ * Link all 'createdBy' and 'updatedBy' fields to Admin (audit tracking)
+ */
+
+// Food
+Admin.hasMany(Food, { foreignKey: 'createdBy' });
+Food.belongsTo(Admin, { as: 'creator', foreignKey: 'createdBy' });
+
+Admin.hasMany(Food, { foreignKey: 'updatedBy' });
+Food.belongsTo(Admin, { as: 'updater', foreignKey: 'updatedBy' });
+
+// FoodCategory
+Admin.hasMany(FoodCategory, { foreignKey: 'createdBy' });
+FoodCategory.belongsTo(Admin, { as: 'creator', foreignKey: 'createdBy' });
+
+Admin.hasMany(FoodCategory, { foreignKey: 'updatedBy' });
+FoodCategory.belongsTo(Admin, { as: 'updater', foreignKey: 'updatedBy' });
+
+// User
+Admin.hasMany(User, { foreignKey: 'createdBy' });
+User.belongsTo(Admin, { as: 'creator', foreignKey: 'createdBy' });
+
+Admin.hasMany(User, { foreignKey: 'updatedBy' });
+User.belongsTo(Admin, { as: 'updater', foreignKey: 'updatedBy' });
+
+// Order
+Admin.hasMany(Order, { foreignKey: 'createdBy' });
+Order.belongsTo(Admin, { as: 'creator', foreignKey: 'createdBy' });
+
+// Order's updatedBy already defined above
+// Keeping it for completeness
+Admin.hasMany(Order, { foreignKey: 'updatedBy' });
+Order.belongsTo(Admin, { as: 'updater', foreignKey: 'updatedBy' });
+
+/**
+ * ===============================
+ * Export All Models
+ * ===============================
+ */
 module.exports = {
   Admin,
   AdminAuditLog,
