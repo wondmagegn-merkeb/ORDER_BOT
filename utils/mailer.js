a02ddeca254a7de73e680e6bcfdbec241ca7f3e7
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-15 20:28:20 +0300

    Create mailer.js

---
+++ utils/mailer.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+const sendMail = async ({ to, subject, html }) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = sendMail;
