e035aa144434b6bbec57dfe577353a9b6a6d7bd9
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-15 14:58:20 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,45 +1,52 @@
 const Admin = require('./Admin');
 const AdminAuditLog = require('./AdminAuditLog');
 const User = require('./User');
+const UserUpdateLog = require('./UserUpdateLog');
 const Food = require('./Food');
 const FoodCategory = require('./FoodCategory');
 const FoodUpdateLog = require('./FoodUpdateLog');
+const Order = require('./Order');
+const OrderUpdateLog = require('./OrderUpdateLog');
 
 // Associations between models
 
 // Admin has many Audit Logs
 Admin.hasMany(AdminAuditLog, { foreignKey: 'adminId' });
-// Each AdminAuditLog belongs to one Admin
 AdminAuditLog.belongsTo(Admin, { foreignKey: 'adminId' });
 
 // FoodCategory has many Foods
 FoodCategory.hasMany(Food, { foreignKey: 'categoryId' });
-// Food belongs to FoodCategory
 Food.belongsTo(FoodCategory, { foreignKey: 'categoryId' });
 
 // Food has many FoodUpdateLogs (for dynamic tracking of changes)
 Food.hasMany(FoodUpdateLog, { foreignKey: 'foodId' });
-// FoodUpdateLog belongs to Food
 FoodUpdateLog.belongsTo(Food, { foreignKey: 'foodId' });
 
-// User has many Orders (or any other models related to user activities)
-User.hasMany(FoodUpdateLog, { foreignKey: 'userId' });  // Example: Assuming a User can update food logs
-FoodUpdateLog.belongsTo(User, { foreignKey: 'userId' });  // Example: FoodUpdateLog is related to a User
+// Associations with Order model
+Order.hasMany(OrderUpdateLog, { foreignKey: 'orderId' });
+OrderUpdateLog.belongsTo(Order, { foreignKey: 'orderId' });
 
-// Order belongs to User (a user can place many orders)
+// User has many Orders
 User.hasMany(Order, { foreignKey: 'userId' });
 Order.belongsTo(User, { foreignKey: 'userId' });
 
-// Order belongs to Food (a food item can appear in many orders)
-Food.hasMany(Order, { foreignKey: 'foodId' });
-Order.belongsTo(Food, { foreignKey: 'foodId' });
+// Admin can update an Order, so add the association without using alias
+Order.belongsTo(Admin, { foreignKey: 'updatedBy' });
+Admin.hasMany(Order, { foreignKey: 'updatedBy' });
+
+// Associations for UserUpdateLog
+User.hasMany(UserUpdateLog, { foreignKey: 'userId' });
+UserUpdateLog.belongsTo(User, { foreignKey: 'userId' });
 
 // Export all models so they can be used in other files
 module.exports = {
   Admin,
   AdminAuditLog,
   User,
+  UserUpdateLog,
   Food,
   FoodCategory,
-  FoodUpdateLog
+  FoodUpdateLog,
+  Order,
+  OrderUpdateLog
 };
