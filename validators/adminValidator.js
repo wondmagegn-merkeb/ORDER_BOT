d2c368e409623d2691d228c4f4587c386fd2a309
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-15 21:35:46 +0300

    Create adminValidator.js

---
+++ validators/adminValidator.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+
+exports.createAdminSchema = Joi.object({
+  username: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  telegramId: Joi.string().required(),
+  role: Joi.string().valid('admin', 'superadmin', 'delivery').required() // added 'delivery' role
+});
+
+exports.loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
+
+exports.updateAdminSchema = Joi.object({
+  username: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  telegramId: Joi.string().required(),
+  role: Joi.string().valid('admin', 'superadmin', 'delivery').required(), // added 'delivery' role
+  states: Joi.string().required() // states made required
+});
+
+exports.forgotPasswordSchema = Joi.object({
+  email: Joi.string().email().required()
+});
+
+exports.resetPasswordSchema = Joi.object({
+  token: Joi.string().required(),
+  newPassword: Joi.string().min(6).required()
+});
