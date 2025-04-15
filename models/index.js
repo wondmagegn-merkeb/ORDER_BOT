bdacb9fff6cd2f1da0419f71f1f29804c6caf3ba
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-15 09:43:22 +0300

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Admin = require('./Admin');
+const AdminAuditLog = require('./AdminAuditLog');
+
+// You can associate models here if needed in future
+
+module.exports = {
+  Admin,
+  AdminAuditLog
+};
