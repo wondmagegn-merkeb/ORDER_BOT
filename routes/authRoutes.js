ac5dea06da28053d5e24de166cdf6392b0ad5f45
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-19 18:52:52 +0300

    Delete routes/authRoutes.js

--- routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-app.get('/', (req, res) => {
-  res.render('home', { title: 'Home Page', layout: false });
-});
-
-app.get('/login', (req, res) => {
-  res.render('login', { message: null, layout: false });
-});
