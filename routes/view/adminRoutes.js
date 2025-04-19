0f8f806403d38bc4aec15a7ebb9a33a97d59d6b4
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-19 18:13:16 +0300

    Update adminRoutes.js

--- routes/view/adminRoutes.js
+++ routes/view/adminRoutes.js
@@ -1,1 +1,10 @@
 
+const express = require('express');
+const router = express.Router();
+const adminViewController = require('../../controllers/view/adminViewController');
+
+router.get('/', adminViewController.listAdmins);
+router.get('/add', adminViewController.showAddForm);
+router.get('/edit/:id', adminViewController.showEditForm);
+
+module.exports = router;
