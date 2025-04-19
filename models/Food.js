9ac030a2179ad128f19c014e721cda01907f5c84
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-19 18:29:46 +0300

    Update Food.js

--- models/Food.js
+++ models/Food.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../../config/db');
+const { sequelize } = require('../config/db');
 const FoodUpdateLog = require('./FoodUpdateLog');
 
 const Food = sequelize.define('Food', {
