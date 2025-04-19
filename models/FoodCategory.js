c2059794a0398ed9cac57c5c184231c6f6eabaab
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-19 18:30:22 +0300

    Update FoodCategory.js

--- models/FoodCategory.js
+++ models/FoodCategory.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../../config/db');
+const { sequelize } = require('../config/db');
 const FoodCategoryUpdateLog = require('./FoodCategoryUpdateLog'); // Import the log model
 
 const FoodCategory = sequelize.define('FoodCategory', {
