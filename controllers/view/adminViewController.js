0d83196e614f59579c7a411288ba2a22d7d43a7c
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-19 18:17:37 +0300

    Update adminViewController.js

--- controllers/view/adminViewController.js
+++ controllers/view/adminViewController.js
@@ -1,1 +1,16 @@
 
+const Admin = require('../../models/Admin');
+
+exports.listAdmins = async (req, res) => {
+  const admins = await Admin.findAll();
+  res.render('admin/list', { admins, title: 'Admin List' });
+};
+
+exports.showAddForm = (req, res) => {
+  res.render('admin/add', { title: 'Add Admin' });
+};
+
+exports.showEditForm = async (req, res) => {
+  const admin = await Admin.findByPk(req.params.id);
+  res.render('admin/edit', { admin, title: 'Edit Admin' });
+};
