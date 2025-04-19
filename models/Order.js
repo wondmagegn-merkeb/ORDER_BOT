1651339343ccf77e5396d2d9d519f5a55c49c339
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-19 18:31:41 +0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../../config/db');
+const { sequelize } = require('../config/db');
 const OrderUpdateLog = require('./OrderUpdateLog'); // Import OrderUpdateLog
 
 // Define the Order model
