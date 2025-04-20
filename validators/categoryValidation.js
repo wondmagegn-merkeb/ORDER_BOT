00fe1d3101ffa9255e7811315bb23f5961b79ffc
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-20 17:19:08 +0300

    Update categoryValidation.js

--- validators/categoryValidation.js
+++ validators/categoryValidation.js
@@ -2,12 +2,11 @@ const Joi = require('joi');
 
 // Joi schema for category validation
 const categoryValidationSchema = Joi.object({
-  name: Joi.string().min(3).required().messages({
+  categoryName: Joi.string().min(3).required().messages({
     'string.base': 'Category name should be a string.',
     'string.min': 'Category name should have at least 3 characters.',
     'any.required': 'Category name is required.'
-  }),
-  description: Joi.string().optional().allow('')
+  })
 });
 
 // Export the schema for use in other files
