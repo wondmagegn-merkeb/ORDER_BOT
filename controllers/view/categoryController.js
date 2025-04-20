85994bbe4a7623b1edac7640d8193e78f4d4849c
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-20 16:34:02 +0300

    Update categoryController.js

--- controllers/view/categoryController.js
+++ controllers/view/categoryController.js
@@ -7,20 +7,20 @@ const {
 exports.listCategories = async (req, res) => {
   try {
     const categories = await getAllCategories();
-    res.render('category', { categories });
+    res.render('category', { categories, title: 'Category List'});
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
 exports.showAddForm = (req, res) => {
-  res.render('addCategory');
+  res.render('addCategory',{title: 'Category Add'});
 };
 
 exports.showEditForm = async (req, res) => {
   try {
-    const category = await getCategoryById(req.params.id);
-    res.render('editCategory', { category });
+    const category = await getCategoryById();
+    res.render('editCategory', { category, title: 'Category Update'});
   } catch (error) {
     res.status(500).send(error.message);
   }
