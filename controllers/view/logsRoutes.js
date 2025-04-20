a8a38541d493a6a0cd51df355765b0dfea477899
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-20 11:57:55 +0300

    Delete controllers/view/logsRoutes.js

--- controllers/view/logsRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const auditLogController = require('../../controllers/view/auditLogController');
-
-// Route to fetch and render audit logs
-router.get('/audit-logs', auditLogController.getAuditLogs);
-
-module.exports = router;
