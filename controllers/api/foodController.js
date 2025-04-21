024498fdc0ae2f47202918c6cd573a57a8d4f972
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-21 10:37:42 +0300

    Update foodController.js

--- controllers/api/foodController.js
+++ controllers/api/foodController.js
@@ -27,7 +27,79 @@ exports.getAllFoods = async (req, res) => {
         price: 10.75,
         isAvailable: true,
         imageUrl: 'https://via.placeholder.com/150'
-      }
+      },
+      {
+        id: 1,
+        name: 'Cheese Pizza',
+        description: 'Delicious cheese pizza with tomato base.',
+        price: 12.99,
+        isAvailable: true,
+        imageUrl: 'https://via.placeholder.com/150'
+      },
+      {
+        id: 2,
+        name: 'Veg Burger',
+        description: 'Loaded with fresh veggies and sauces.',
+        price: 8.5,
+        isAvailable: false,
+        imageUrl: 'https://via.placeholder.com/150'
+      },
+      {
+        id: 3,
+        name: 'Pasta Alfredo',
+        description: 'Creamy Alfredo pasta with garlic bread.',
+        price: 10.75,
+        isAvailable: true,
+        imageUrl: 'https://via.placeholder.com/150'
+      },
+     {
+        id: 1,
+        name: 'Cheese Pizza',
+        description: 'Delicious cheese pizza with tomato base.',
+        price: 12.99,
+        isAvailable: true,
+        imageUrl: 'https://via.placeholder.com/150'
+      },
+      {
+        id: 2,
+        name: 'Veg Burger',
+        description: 'Loaded with fresh veggies and sauces.',
+        price: 8.5,
+        isAvailable: false,
+        imageUrl: 'https://via.placeholder.com/150'
+      },
+      {
+        id: 3,
+        name: 'Pasta Alfredo',
+        description: 'Creamy Alfredo pasta with garlic bread.',
+        price: 10.75,
+        isAvailable: true,
+        imageUrl: 'https://via.placeholder.com/150'
+      },
+     {
+        id: 1,
+        name: 'Cheese Pizza',
+        description: 'Delicious cheese pizza with tomato base.',
+        price: 12.99,
+        isAvailable: true,
+        imageUrl: 'https://via.placeholder.com/150'
+      },
+      {
+        id: 2,
+        name: 'Veg Burger',
+        description: 'Loaded with fresh veggies and sauces.',
+        price: 8.5,
+        isAvailable: false,
+        imageUrl: 'https://via.placeholder.com/150'
+      },
+      {
+        id: 3,
+        name: 'Pasta Alfredo',
+        description: 'Creamy Alfredo pasta with garlic bread.',
+        price: 10.75,
+        isAvailable: true,
+        imageUrl: 'https://via.placeholder.com/150'
+      }, 
     ];
 
     return foods; // Respond with demo data
