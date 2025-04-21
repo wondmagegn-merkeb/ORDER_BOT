7a1a121301b0406574acd4e477bd171520dfbf7d
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-21 10:22:54 +0300

    Update foodController.js

--- controllers/view/foodController.js
+++ controllers/view/foodController.js
@@ -3,7 +3,7 @@ const { getAllFoods } = require('../api/foodController');
 exports.renderFoodList = async (req, res) => {
   try {
     const foods = await getAllFoods(); // Will return JSON from API controller
-    res.render('admin/food/list-food', { foods });
+    res.render('admin/food/list-food', { foods ,title:'Food List'});
   } catch (error) {
     res.status(500).send('Unable to load food items.');
   }
