c7b8e6acfe88218e75e84d140bb2552a73685176
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-22 11:33:05 +0300

    Update foodRoutes.js

--- routes/api/foodRoutes.js
+++ routes/api/foodRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const foodController = require('../../controllers/api/foodController');
+const upload = require("../../middleware/uploadMiddleware");
 
-router.get('/', foodController.getAllFoods);
+router.post("/", upload.single("image"), foodController.createFood);
+router.get("/", foodController.getAllFoods);
+router.get("/:id", foodController.getFoodById);
+router.put("/:id", upload.single("image"), foodController.updateFood);
+router.delete("/:id", foodController.deleteFood);
 
 module.exports = router;
