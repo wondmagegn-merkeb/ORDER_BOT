82ad2c40eb600ea2494edecbd957a6f4164f6a5e
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-22 15:31:23 +0300

    Create userRoutes.js

---
+++ routes/view/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../../controllers/view/userController');
+
+// Route to list all categories in the view
+router.get('/', userController.listUsers);
+
+// Route to show the form to edit an existing category
+router.get('/edit/:id', userController.showEditForm);
+
+module.exports = router;
