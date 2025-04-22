5b0ac6d9175d240e4ae8c1d1f0779fbaf95826f3
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-22 09:04:25 +0300

    Update foodRoutes.js

--- routes/view/foodRoutes.js
+++ routes/view/foodRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const foodViewController = require('../../controllers/view/foodController');
 
 router.get('/', foodViewController.renderFoodList);
-router.get('/add', (req, res) => res.render('admin/food/foodForm', { title:'Food Add', food: null, categories: ['Pizza', 'Burger', 'Pasta', 'Drinks'] }));
+router.get('/add', foodViewController.renderCreateForm); // Show form
 module.exports = router;
