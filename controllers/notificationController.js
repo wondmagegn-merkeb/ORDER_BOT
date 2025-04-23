8c8ecc784b78c1cb58d9c937f6a5615c08a3fc10
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-23 17:30:41 +0300

    Create notificationController.js

---
+++ controllers/notificationController.js
@@ -0,0 +1,20 @@
+const { sendMessageToUser }= require('../bots/userBot');
+
+async function notifyUserController() {
+    const telegramId = 7816314576;
+    const message = '📢 This is a message from the bot controller!';
+
+    if (!telegramId) {
+        return ctx.reply('❌ Could not determine your Telegram ID.');
+    }
+
+    try {
+        await sendMessageToUser(telegramId, message);
+        
+    } catch (err) {
+        console.error('Failed to notify user:', err);
+        
+    }
+}
+
+module.exports = { notifyUserController };
