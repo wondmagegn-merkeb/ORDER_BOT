8a4248cc6abf80b42198d673a532cc25b4125b66
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-23 08:05:41 +0300

    Update b4cyakpniraejxy0wee5 (1).sql

--- b4cyakpniraejxy0wee5 (1).sql
+++ b4cyakpniraejxy0wee5 (1).sql
@@ -19,3 +19,20 @@ INSERT INTO `food_category_update_logs` (`id`, `categoryId`, `oldValue`, `newVal
 INSERT INTO `users` (`userId`, `telegramId`, `username`, `status`, `userType`, `updatedBy`, `createdAt`, `updatedAt`, `deletedAt`) VALUES
 ('USR001', '7816314576', NULL, 'active', 'customer', NULL, '2025-04-22 13:42:01', '2025-04-22 13:42:01', NULL);
 
+INSERT INTO `foods` (`foodId`, `name`, `description`, `price`, `isAvailable`, `imageUrl`, `categoryId`, `cloudinaryPublicId`, `createdBy`, `updatedBy`, `createdAt`, `updatedAt`, `deletedAt`) VALUES
+('FOOD001', 'Cheeseburger', 'My name is bla bla', 21, 1, 'https://res.cloudinary.com/dx6koeeva/image/upload/v1745345293/foods/jraymwjpwxx3jsxmeha3.webp', 'CAT001', 'foods/jraymwjpwxx3jsxmeha3', 'ADM001', 'ADM001', '2025-04-22 18:08:14', '2025-04-22 18:08:14', NULL),
+('FOOD002', 'Cheeseburge', 'My name is bla bla', 21, 1, 'https://res.cloudinary.com/dx6koeeva/image/upload/v1745346462/foods/j6eelrbpqjp87otd5fpp.webp', 'CAT001', 'foods/j6eelrbpqjp87otd5fpp', 'ADM001', 'ADM001', '2025-04-22 18:27:43', '2025-04-22 18:27:43', NULL);
+
+INSERT INTO `food_update_logs` (`id`, `foodId`, `field`, `oldValue`, `newValue`, `performedBy`, `action`, `createdAt`, `updatedAt`) VALUES
+(1, 'FOOD001', 'name', NULL, 'Cheeseburger', 'ADM001', 'create', '2025-04-22 18:08:14', '2025-04-22 18:08:14'),
+(2, 'FOOD001', 'description', NULL, 'My name is bla bla', 'ADM001', 'create', '2025-04-22 18:08:14', '2025-04-22 18:08:14'),
+(3, 'FOOD001', 'price', NULL, '21', 'ADM001', 'create', '2025-04-22 18:08:14', '2025-04-22 18:08:14'),
+(4, 'FOOD001', 'isAvailable', NULL, 'true', 'ADM001', 'create', '2025-04-22 18:08:14', '2025-04-22 18:08:14'),
+(5, 'FOOD001', 'imageUrl', NULL, 'https://res.cloudinary.com/dx6koeeva/image/upload/v1745345293/foods/jraymwjpwxx3jsxmeha3.webp', 'ADM001', 'create', '2025-04-22 18:08:14', '2025-04-22 18:08:14'),
+(6, 'FOOD001', 'categoryId', NULL, 'CAT001', 'ADM001', 'create', '2025-04-22 18:08:14', '2025-04-22 18:08:14'),
+(7, 'FOOD002', 'name', NULL, 'Cheeseburge', 'ADM001', 'create', '2025-04-22 18:27:43', '2025-04-22 18:27:43'),
+(8, 'FOOD002', 'description', NULL, 'My name is bla bla', 'ADM001', 'create', '2025-04-22 18:27:43', '2025-04-22 18:27:43'),
+(9, 'FOOD002', 'price', NULL, '21', 'ADM001', 'create', '2025-04-22 18:27:43', '2025-04-22 18:27:43'),
+(10, 'FOOD002', 'isAvailable', NULL, 'true', 'ADM001', 'create', '2025-04-22 18:27:43', '2025-04-22 18:27:43'),
+(11, 'FOOD002', 'imageUrl', NULL, 'https://res.cloudinary.com/dx6koeeva/image/upload/v1745346462/foods/j6eelrbpqjp87otd5fpp.webp', 'ADM001', 'create', '2025-04-22 18:27:43', '2025-04-22 18:27:43'),
+(12, 'FOOD002', 'categoryId', NULL, 'CAT001', 'ADM001', 'create', '2025-04-22 18:27:43', '2025-04-22 18:27:43');
