d02cf5547601da1402ac7351bb2689c6a018da71
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-23 17:56:37 +0300

    Update userBot.js

--- bots/userBot.js
+++ bots/userBot.js
@@ -23,55 +23,6 @@ const userBot = new Telegraf(process.env.USER_BOT_TOKEN);
 
 // Session support
 userBot.use(new LocalSession({ database: 'session_db.json' }).middleware());
-async function placeOrderOne(ctx) { 
-    const imagePath = path.join(__dirname, '../../public', 'welcome.png');
-    const imageExists = fs.existsSync(imagePath);
-
-    const adminCaption = `<b>📦 *New Order Received!*</b>\n`;
-
-    const admins = await Admin.findAll();
-    const adminTelegramIds = admins.map(admin => admin.telegramId);
-
-    for (const telegramId of adminTelegramIds) {
-        try {
-            if (imageExists) {
-                await userBot.telegram.sendPhoto(
-                    telegramId,
-                    { source: fs.createReadStream(imagePath) },
-                    {
-                        caption: adminCaption,
-                        parse_mode: 'HTML',
-                        reply_markup: {
-                            inline_keyboard: [
-                                [{ text: "📋 View Details", callback_data: `view_order_food` }],
-                                [{ text: "✅ Confirm Order", callback_data: `confirm_order` }],
-                                [{ text: "❌ Cancel Order", callback_data: `cancel_order` }]
-                            ]
-                        }
-                    }
-                );
-            } else {
-                await userBot.telegram.sendMessage(
-                    telegramId,
-                    adminCaption,
-                    {
-                        parse_mode: 'HTML',
-                        reply_markup: {
-                            inline_keyboard: [
-                                [{ text: "📋 View Details", callback_data: `view_order_food` }],
-                                [{ text: "✅ Confirm Order", callback_data: `confirm_order` }],
-                                [{ text: "❌ Cancel Order", callback_data: `cancel_order` }]
-                            ]
-                        }
-                    }
-                );
-            }
-        } catch (err) {
-            console.error(`❌ Failed to send to user ${telegramId}:`, err);
-            await ctx.reply(`❌ Failed to send to user ${telegramId}:`+err);
-        }
-    }
-}
 
 // /start command
 userBot.start(async (ctx) => {
@@ -315,4 +266,4 @@ async function sendMessageToUser(telegramId, message, parseMode = 'HTML') {
     }
 }
 
-module.exports = { userBot, placeOrderOne, sendMessageToUser};
+module.exports = { userBot, sendMessageToUser};
