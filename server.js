0d2ac9f0f6033cb1eef2c606693433544a75ea90
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-24 11:01:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ app.use(globalErrorHandler);
     await sequelize.authenticate();
     console.log('✅ Database connected');
 
-    await sequelize.sync({ alter : true });
+    await sequelize.sync({ force : true });
 
     const PORT = process.env.PORT || 8080;
 
