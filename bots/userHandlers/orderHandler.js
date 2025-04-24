24edd8b8adec0a1ba4aeae2fb63fb770122767bd
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-24 07:26:21 +0300

    Update orderHandler.js

--- bots/userHandlers/orderHandler.js
+++ bots/userHandlers/orderHandler.js
@@ -1,7 +1,7 @@
 const { Order, Food, User, Admin } = require('../../models/index');
 const { adminBot } = require('../adminBot'); // Adjust the path based on your project structure
 const { Op } = require('sequelize');
-
+const { Markup } = require('telegraf');
 async function placeOrder(ctx, foodId) {
     const telegramId = ctx.from.id.toString();
 
