2d8daed1cdd5f70cf928a73d3256bebac7e3851f
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-25 12:55:52 +0300

    Update adminRoutes.js

--- routes/api/adminRoutes.js
+++ routes/api/adminRoutes.js
@@ -5,6 +5,6 @@ const adminController = require('../../controllers/api/adminController');
 
 router.get('/', adminController.getAllAdmins);
 router.post('/', adminController.createAdmin);
-router.put('/:id', adminController.updateAdmin);
+router.post('/:id', adminController.updateAdmin);
 
 module.exports = router;
