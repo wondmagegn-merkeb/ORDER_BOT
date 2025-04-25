68258f1468ceafccdcd9cf261b55020edf34c0a8
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-25 13:43:27 +0300

    Update foodRoutes.js

--- routes/view/foodRoutes.js
+++ routes/view/foodRoutes.js
@@ -4,4 +4,5 @@ const foodViewController = require('../../controllers/view/foodController');
 
 router.get('/', foodViewController.renderFoodList);
 router.get('/add', foodViewController.renderCreateForm); // Show form
+router.get('/edit/:id', foodViewController.renderUpdateForm); // Show form
 module.exports = router;
