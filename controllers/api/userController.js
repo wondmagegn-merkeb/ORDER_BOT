2ac04a9f0e461dc9282309f8fff4323d7c1bfbd5
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-26 08:12:45 +0300

    Update userController.js

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,7 +1,7 @@
 const { User } = require('../../models/index');
 const { InternalServerError } = require('../../utils/customError');
 const userValidationSchema = require('../../validators/userValidation'); // Assuming you have a validation schema
-
+const { sendMessageToUser }= require('../../bots/userBot');
 // Get all users
 exports.getAllUsers = async (req, res, next) => {
   try {
@@ -25,14 +25,12 @@ exports.getUserById = async (userId) => {
 // Update user
 exports.updateUser = async (req, res, next) => {
   try {
-    // Validate input using userValidationSchema
     const { error } = userValidationSchema.validate(req.body);
-    
     const { status, userType } = req.body;
     const userId = req.params.id;
-    
+
     const userData = { status, userType, userId };
-    
+
     if (error) {
       res.locals.error = error.details[0].message;
       return res.render('admin/user/update-user', {
@@ -50,12 +48,35 @@ exports.updateUser = async (req, res, next) => {
       });
     }
 
-    // Update user fields
+    const statusChanged = user.status !== status;
+
+    // Update fields
     user.status = status;
     user.userType = userType;
     user.updatedBy = req.admin.adminId;
 
     await user.save();
+
+    if (statusChanged) {
+      const generateUserUpdateMessage = (user) => {
+        const statusNote = user.status === 'inactive'
+          ? '⚠️ You are now <b>blocked</b> from accessing the system.'
+          : '✅ You are now <b>active</b> and can use the system.';
+
+        return `
+<b>⚙️ User Updated!</b>
+
+<b>Full Name:</b> ${user.fullName || 'Not provided'}
+<b>Username:</b> ${user.username || 'Not provided'}
+<b>User Type:</b> ${user.userType || 'Not provided'}
+<b>Status:</b> ${user.status || 'Not provided'}
+
+${statusNote}
+        `;
+      };
+
+      await sendMessageToUser(user.telegramId, generateUserUpdateMessage(user));
+    }
 
     res.locals.success = 'User updated successfully!';
     return res.render('admin/user/update-user', {
