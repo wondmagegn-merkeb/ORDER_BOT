f8da0766c7402013e528617392301f7f97cb181a
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-26 11:57:22 +0300

    Update logsRoutes.js

--- routes/view/logsRoutes.js
+++ routes/view/logsRoutes.js
@@ -6,8 +6,8 @@ const auditLogController = require('../../controllers/view/auditLogController');
 // Route to fetch and render audit logs
 router.get('/admin', auditLogController.getAdminLogs);
 router.get('/user', auditLogController.getUserLogs);
-router.get('/orders', auditLogController.getOrdersLogs);
-router.get('/items', auditLogController.getItemsLogs);
-router.get('/categories', auditLogController.getCategoriesLogs);
+router.get('/orders', auditLogController.getOrderLogs);
+router.get('/items', auditLogController.getFoodLogs);
+router.get('/categories', auditLogController.getCategoryLogs);
 
 module.exports = router;
