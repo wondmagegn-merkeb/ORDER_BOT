833594e0c5ba68f773e118f6c2f6201a87bdd932
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-26 20:24:56 +0300

    Update orderRoutes.js

--- routes/api/orderRoutes.js
+++ routes/api/orderRoutes.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const orderController = require('../../controllers/api/orderController'); // Adjust path to your actual order controller
-
-// API endpoint to get all orders
-router.get('/', orderController.getAllOrders);
-
-// API endpoint to get an order by ID
-router.get('/:id', orderController.getOrderById);
+const orderController = require('../../controllers/api/orderController'); 
 
 // API endpoint to update an existing order
-router.post('/:id', orderController.updateOrder); // You can change to .put() if your frontend/client uses PUT
+router.post('/:id', orderController.updateOrder); /
 
 module.exports = router;
