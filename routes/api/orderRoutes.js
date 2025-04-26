759a95342376e997b25a81595d07572717be1ac6
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-26 23:02:37 +0300

    Update orderRoutes.js

--- routes/api/orderRoutes.js
+++ routes/api/orderRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const orderController = require('../../controllers/api/orderController'); 
 
 // API endpoint to update an existing order
-router.post('/:id', orderController.updateOrder); /
+router.post('/:id', orderController.updateOrder); 
 
 module.exports = router;
