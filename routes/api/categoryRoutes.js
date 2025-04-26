d5ccb63353c7aa52083fc747a910a0c5699417cb
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-26 20:55:51 +0300

    Update categoryRoutes.js

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -2,19 +2,8 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../../controllers/api/categoryController'); // Adjust path if necessary
 
-// API endpoint to get all categories
-router.get('/', categoryController.getAllCategories);
-
-// API endpoint to get a category by ID
-router.get('/:id', categoryController.getCategoryById);
-
-// API endpoint to create a new category
 router.post('/', categoryController.createCategory);
-
-// API endpoint to update an existing category
 router.post('/:id', categoryController.updateCategory);
-
-// API endpoint to delete a category
-router.delete('/:id', categoryController.deleteCategory);
+router.post('/delete/:id', categoryController.deleteCategory);
 
 module.exports = router;
