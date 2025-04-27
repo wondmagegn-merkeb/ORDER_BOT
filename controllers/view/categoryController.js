fa1e9b8fc48265a264644b3ce72ce9d52c8d80ad
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-27 05:40:03 +0300

    Update categoryController.js

--- controllers/view/categoryController.js
+++ controllers/view/categoryController.js
@@ -4,6 +4,7 @@ const {
 } = require('../api/categoryController');
 const { InternalServerError } = require('../../utils/customError');
 
+// List all categories
 exports.listCategories = async (req, res, next) => {
   try {
     const categories = await getAllCategories();
@@ -12,11 +13,10 @@ exports.listCategories = async (req, res, next) => {
     const modelColumns = [
       { name: 'Category ID', field: 'categoryId', index: 0 },
       { name: 'Category Name', field: 'categoryName', index: 1 },
-      { name: 'Description', field: 'description', index: 2 }
+      { name: 'Description', field: 'description', index: 2 },
     ];
 
-const filters = [];
-
+    const filters = [];
 
     res.render('admin/category/list-category', {
       title: 'Category List',
@@ -29,28 +29,24 @@ const filters = [];
         canView: false,
         canAdd: true,
         canEdit: true,
-        canDelete:true
-          }
+        canDelete: true,
+      },
     });
-    
   } catch (error) {
     next(new InternalServerError('Failed to list categories', error));
   }
 };
 
+// Show form to add a new category
 exports.showAddForm = (req, res) => {
   res.render('admin/category/create-category', { title: 'Category Add' });
 };
 
+// Show form to edit a category
 exports.showEditForm = async (req, res, next) => {
   try {
     const categoryId = req.params.id;
-    
     const category = await getCategoryById(categoryId);
-
-    if (!category) {
-      return res.status(404).send('Category not found');
-    }
 
     res.render('admin/category/update-category', { category, title: 'Category Update' });
   } catch (error) {
