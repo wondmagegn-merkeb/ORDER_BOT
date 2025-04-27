4b73d7a16edfd11cbc194bfa97ebada03bdfd801
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-27 05:18:26 +0300

    Update orderRoutes.js

--- routes/view/orderRoutes.js
+++ routes/view/orderRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../../controllers/view/orderController');
 
-// Route to list all orders in the view
 router.get('/', orderController.listOrders);
-
-// Route to show the form to edit an existing order
 router.get('/edit/:id', orderController.showEditOrderForm);
 
 module.exports = router;
