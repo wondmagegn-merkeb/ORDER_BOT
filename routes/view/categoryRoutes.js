7598a964ff410678d5bd1afbc25e679ec48c810e
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-27 05:17:28 +0300

    Update categoryRoutes.js

--- routes/view/categoryRoutes.js
+++ routes/view/categoryRoutes.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../../controllers/view/categoryController');
 
-// Route to list all categories in the view
 router.get('/', categoryController.listCategories);
-
-// Route to show the form to add a new category
 router.get('/add', categoryController.showAddForm);
-
-// Route to show the form to edit an existing category
 router.get('/edit/:id', categoryController.showEditForm);
 
 module.exports = router;
