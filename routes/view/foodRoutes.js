b66e8e4b0588de2340f2d0bdfcc261af1970e300
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-27 05:16:38 +0300

    Update foodRoutes.js

--- routes/view/foodRoutes.js
+++ routes/view/foodRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const foodViewController = require('../../controllers/view/foodController');
 
 router.get('/', foodViewController.renderFoodList);
-router.get('/add', foodViewController.renderCreateForm); // Show form
-router.get('/edit/:id', foodViewController.renderUpdateForm); // Show form
+router.get('/add', foodViewController.renderCreateForm); 
+router.get('/edit/:id', foodViewController.renderUpdateForm); 
 module.exports = router;
