c313d05d48fd9fad947f84d58df439630d648e0f
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-30 21:27:19 +0300

    Update menuHandler.js

--- bots/userHandlers/menuHandler.js
+++ bots/userHandlers/menuHandler.js
@@ -50,5 +50,54 @@ async function getMenu(ctx) {
         await ctx.reply('<b>🚨 Error loading the menu. Please try again later.</b>', { parse_mode: 'HTML' });
     }
 }
+async function getMenuByCategory(ctx,categoryId) {
+    try {
+        const foods = await Food.findAll({
+            where: { isAvailable: true, categoryId},
+            include: {
+                model: FoodCategory,
+                attributes: ['categoryName'],
+            }
+        });
 
-module.exports = { getMenu };
+        if (foods.length === 0) {
+            return ctx.reply('🚫 <b>No food items available right now.</b>', { parse_mode: 'HTML' });
+        }
+
+        for (const food of foods) {
+            const category = food.FoodCategory.categoryName;
+
+            const foodDetails = `
+🍽 <b>${food.name}</b>
+📂 <i>Category:</i> ${category}
+💰 <i>Price:</i> ${food.price} birr
+📝 <i>Description:</i> ${food.description || 'No description'}
+✅ <i>Available</i>
+            `.trim();
+
+            const keyboard = {
+                inline_keyboard: [
+                    [{ text: '🛒 Order Now', callback_data: `order_now_${food.foodId}` }]
+                ]
+            };
+
+            if (food.imageUrl && food.imageUrl.startsWith('http')) {
+                await ctx.replyWithPhoto(food.imageUrl, {
+                    caption: foodDetails,
+                    parse_mode: 'HTML',
+                    reply_markup: keyboard
+                });
+            } else {
+                await ctx.reply(foodDetails, {
+                    parse_mode: 'HTML',
+                    reply_markup: keyboard
+                });
+            }
+        }
+    } catch (error) {
+        console.error('❌ Error displaying menu by catagory:', error);
+        await ctx.reply('<b>🚨 Error loading the menu by catagory. Please try again later.</b>', { parse_mode: 'HTML' });
+    }
+}
+
+module.exports = { getMenu, getMenuByCategory};
