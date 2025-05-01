3437cddfa9ab24fa147fa96c0bbfcbeddd4b4bed
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-05-01 18:43:31 +0300

    Update foodController.js

--- controllers/view/foodController.js
+++ controllers/view/foodController.js
@@ -1,37 +1,34 @@
-const { getAllFoods } = require('../api/foodController');
+const { getAllFoods ,getFoodById} = require('../api/foodController');
 const {
-  getAllCategories,
-  getFoodById
+  getAllCategories
 } = require('../api/categoryController');
 const { InternalServerError } = require('../../utils/customError');
 
-exports.renderFoodList = async (req, res) => {
+exports.renderFoodList = async (req, res, next) => {
   try {
-    const foods = await getAllFoods(); // Will return JSON from API controller
-    res.render('admin/food/list-food', { foods ,title:'Food List'});
+    const foods = await getAllFoods(); // Get all foods
+    res.render('admin/food/list-food', { foods, title: 'Food List' });
   } catch (err) {
     next(new InternalServerError('Error loading food items.', err));
   }
 };
 
-exports.renderCreateForm = async (req, res) => {
+exports.renderCreateForm = async (req, res, next) => {
   try {
-    const categories = await getAllCategories();
-    res.render('admin/food/create-food', { categories,title:'Food List' });
+    const categories = await getAllCategories(); // Get all categories for dropdown
+    res.render('admin/food/create-food', { categories, title: 'Create Food' });
   } catch (err) {
     next(new InternalServerError('Error loading create form.', err));
   }
 };
 
-exports.renderUpdateForm = async (req, res) => {
+exports.renderUpdateForm = async (req, res, next) => {
   try {
-    const foodId =req.params.id;
-    const food = await getFoodById(foodId);
-    const categories = await getAllCategories();
-    res.render('admin/food/list-food', { foods ,title:'Food List'});
-  //  res.render('admin/food/update-food', { categories,food,title:'Food Update' });
+    const foodId = req.params.id;
+    const food = await getFoodById(foodId); // Get food by ID
+    const categories = await getAllCategories(); // Get all categories for dropdown
+    res.render('admin/food/update-food', { food, categories, title: 'Food Update' });
   } catch (err) {
     next(new InternalServerError('Error loading edit form.', err));
   }
 };
-
